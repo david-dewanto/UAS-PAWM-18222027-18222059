31947a7ae8984e6eea5c9f4e9b9be43a849e21ec
David Dewanto <david.dewanto@gmail.com>
2024-12-08 16:50:22 +0700

    NPX Restart

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import LottieView from 'lottie-react-native';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
